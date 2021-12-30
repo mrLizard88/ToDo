ba9e498e28a961606a2981e2d52c0c4713c8335d
mrLizard88 <jakubjanyst@gmail.com>
2021-12-30 20:55:02 +0100

    first commit

---
+++ ToDo.py
@@ -0,0 +1,29 @@
+import ToDo_Class
+
+if __name__ == "__main__":
+
+    lista = ToDo_Class.lista_zadan('C:/plik.pcl')
+
+    while True:
+        polecenie = int(input("""1 - Dodaj zadanie
+2 - Skasuj zadania
+3 - Wyświetl zadania
+0 - Zakończ program
+"""))
+        if polecenie == 1:
+            priorytet = input("Podaj priorytet: ")
+            opis = input("Podaj opis: ")
+            lista.dodaj_zadanie(priorytet, opis)
+
+        elif polecenie == 2:
+            skasuj = int(input("Które zadanie chcesz skasować?\n"))
+            lista.usun_zadanie(skasuj)
+
+        elif polecenie == 3:
+            print("Twoje zadania:\n")
+            lista.wyswietl_zadania()
+
+        elif polecenie == 0:
+            print("Koniec programu")
+            lista.zapisz_listę()
+            break
---
+++ ToDo_Class.py
@@ -0,0 +1,50 @@
+import pickle
+from datetime import datetime, timedelta
+
+class zadanie:
+
+    def __init__(self, numer_zadania, priorytet, opis):
+        self.data = datetime.now()
+        self.numer_zadania = numer_zadania
+        self.priorytet = priorytet
+        self.opis = opis
+
+    def __str__(self):
+        data_utworzenia = datetime.strftime(self.data, "%Y-%m-%d")
+        dni_temu = (datetime.now() - self.data).days
+
+        return f" Zadanie: {self.numer_zadania}\nPriorytet: {self.priorytet}\nOpis: {self.opis}\nCzas utworzenia: {data_utworzenia}\nDni temu: {dni_temu}\n"
+
+class lista_zadan:
+
+    lista = []
+    numer_zadania = 0
+
+    def __init__(self, plik):
+        self.plik = plik
+        try:
+            with open (self.plik, "rb") as f:
+                self.lista = pickle.load(f)
+                print("Czytam")
+                self.numer_zadania = max(x.numer_zadania for x in self.lista)
+        except:
+            print("Plik z zadaniami nie istnieje!")
+
+    def wyswietl_zadania(self):
+        for x in self.lista:
+            print(x)
+
+    def dodaj_zadanie(self, priorytet, opis):
+        self.numer_zadania += 1
+        nowe_zadanie = zadanie(self.numer_zadania, priorytet, opis)
+        self.lista.append(nowe_zadanie)
+
+    def usun_zadanie(self, skasuj):
+        for a, b in enumerate(self.lista):
+            if b.numer_zadania == int(skasuj):
+                del self.lista[a]
+                return
+
+    def zapisz_listę(self):
+        with open (self.plik, "wb") as f:
+            pickle.dump(self.lista, f)
